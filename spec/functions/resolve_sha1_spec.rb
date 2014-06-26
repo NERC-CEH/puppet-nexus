7af3bb9a7714543a8a9534cfd447889f783b60d3
root <root@vagrant.vm>
2014-06-26 17:26:36 +0100

    Adding an extra test

--- spec/functions/resolve_sha1_spec.rb
+++ spec/functions/resolve_sha1_spec.rb
@@ -15,7 +15,25 @@ describe "resolve_sha1" do
                                           '0.1',
                                           'jar'])
     expect(result).to(eq('f645a04f5354ff2251fc9f5a7de86a510d7faf1c'))
-
   end
 
+  it "should fail if the artifact cant be located" do
+    expect {scope.function_resolve_sha1('https://oss.sonatype.org/service/local',
+                                        'public',
+                                        'some.nonsense',
+                                        'invalid',
+                                        '0.1',
+                                        'jar') }
+    .to( raise_error(ArgumentError) )
+  end
+
+  it "should fail if the nexus server does not exist" do
+    expect {scope.function_resolve_sha1('some/random/nexus/server',
+                                        'public',
+                                        'some.nonsense',
+                                        'invalid',
+                                        '0.1',
+                                        'jar') }
+    .to( raise_error(ArgumentError) )
+  end    
 end
